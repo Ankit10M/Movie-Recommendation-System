6ec9bdfc1eef6db1722ea777523a9e68b6af9dc3
Ankit <ankitbrijeshmishra10@gmail.com>
2026-02-17 00:17:14 +0530

    backend url added to streamlit

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import requests
 import streamlit as st
 
 #  CONFIG
-API_BASE = "http://127.0.0.1:8000"
+API_BASE = 'https://movie-recommendation-system-hk25.onrender.com' or "http://127.0.0.1:8000"
 TMDB_IMG = "https://image.tmdb.org/t/p/w500"
 
 st.set_page_config(page_title="Movie Recommender", page_icon="🎬", layout="wide")
